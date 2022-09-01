dc9a32419d78c1001b9e96b29d7a153e20537e75
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-01 20:12:54 +0200

    feat: configured multer to save files with extensions

--- src/lib/middleware/multer.ts
+++ src/lib/middleware/multer.ts
@@ -1,8 +1,20 @@
 import multer from "multer";
+import mime from "mime";
+import { randomUUID } from "crypto";
 
 const storage = multer.diskStorage({
-    destination: "uploads/"
+    destination: "uploads/",
+    filename: (req, file, callback) => {
+        return callback(null, generateFileName(file.mimetype))
+    }
 })
+
+export const generateFileName = (mimeType: string) => {
+    const randomFileName = `${randomUUID()}-${Date.now()}`
+    const fileExtension = mime.getExtension(mimeType)
+    const fileName = `${randomFileName}.${fileExtension}`
+    return fileName
+}
 
 export const multerOptions = {
 
